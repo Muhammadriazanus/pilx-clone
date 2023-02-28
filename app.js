fec13c0d83c47323375f1f81058b0c934f515425
muhammadriazanus <riazanus568@gmail.com>
2023-02-28 02:26:43 -0800

    second numerical

---
+++ app.js
@@ -0,0 +1,44 @@
+// function sumOfFourNumber (a,b,c,d){
+//     // return (`the sum of four number is ${sumOfFourNumber}`)
+//     return a+b+c+d
+
+// }
+// alert(sumOfFourNumber(1,23,4,55))
+
+// function expression  
+
+// const sumOfFourNumber = function(a,b,c,d){
+    // return  a+b+c+d
+// }
+
+function add(a,b,x){
+    var z = a+b+x;
+    return z;
+}
+var g  = add(3,4,5)
+alert(g)
+
+
+// function fullName(parameter){
+//     for(let i = 0; i<=5;i++)
+//     alert("hello world")
+// }
+// fullName(arrgument)
+
+// parameter universal area whi
+// function first(b,a){
+//     return (`the sum of b and a${b + a }`)
+// }
+// console.log(first(24,5))
+// var firstname  = prompt("ENter your first name")
+// var lastname = prompt("Enter your last name")
+
+// function fullName(firstname,lastname){
+//     return(`my full name is ${firstname} ${lastname}`)
+// }
+// console.log(fullName(firstname,lastname))
+
+function first(b,a){
+    return "hello"
+}
+console.log(first(2,3))
